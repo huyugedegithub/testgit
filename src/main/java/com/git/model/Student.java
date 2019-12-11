6c4e18be7aa0d89b4ead8716e11751198dc3478c
Administrator <3193598635@qq.com>
2019-12-11 21:43:41 +0800

    工程测试

---
+++ src/main/java/com/git/model/Student.java
@@ -0,0 +1,13 @@
+package com.git.model;
+
+/**
+ * @Description:
+ * @Author huyuge
+ * @Date 2019/12/11 0011 下午 21:40
+ * @Version 1.0
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("123456789");
+    }
+}
